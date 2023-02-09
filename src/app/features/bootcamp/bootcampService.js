fbbf926e74dea0ba03bb2c3e5281fe35468f4591
temi-test <temidaramola6@gmail.com>
2023-02-09 09:03:39 +0100

    added remote api url to auth and bootcamp services

--- src/app/features/bootcamp/bootcampService.js
+++ src/app/features/bootcamp/bootcampService.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:5000/";
+// const BASE_URL = "http://localhost:5000/";
+const BASE_URL = "https://prodezign-backend.onrender.com/";
+
+
 
 const getBootcamp = async (payload) => {
   const response = await axios.get(BASE_URL + "bootcamp");
